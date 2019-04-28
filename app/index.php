93be4d3ecc5fc56494eabd67468b641564d9bc28
gtestault <gabriel@testault.com>
2019-04-28 21:02:52 +0200

    WIP: shitty php app

--- app/index.php
+++ app/index.php
@@ -1,8 +1,34 @@
-<html>
- <head>
-  <title>PHP-Test</title>
- </head>
- <body>
- <?php echo '<p>Hallo Welt</p>'; ?>
- </body>
-</html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, "sakila");
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+if ($result = $conn->query("SELECT * FROM actor LIMIT 10")) {
+    printf("Select returned %d rows.\n", $result->num_rows);
+
+    /* free result set */
+    $result->close();
+}
+?> 
+
+<form action="" method="post">
+<table width="50%">
+    <tr>
+        <td>User</td>
+        <td><input type="text" name="user"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><input type="text" name="password"></td>
+    </tr>
+</table>
+    <input type="submit" value="OK" name="s">
+</form>
